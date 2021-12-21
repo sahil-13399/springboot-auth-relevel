fb04d72d97725f126b5150401d3eb9f0128e89c5
sahil.samantaray <sahil.samantaray@nymble.in>
2021-12-21 11:45:55 +0530

    Added JWT files

--- src/main/java/com/relevel/backend/service/UserService.java
+++ src/main/java/com/relevel/backend/service/UserService.java
@@ -2,23 +2,37 @@ package com.relevel.backend.service;
 
 import com.relevel.backend.dto.User;
 import com.relevel.backend.repository.UserRepository;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
   @Autowired
   UserRepository userRepository;
 
-
-  public String registerUser(User user) {
-  try {
-    userRepository.save(user);
-  } catch (Exception e) {
-    e.printStackTrace();
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
   }
 
+  public String registerUser(User user) {
+    try {
+      userRepository.save(user);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
     return "Successful";
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(username);
+    return new org.springframework.security.core.userdetails.User(user.getEmail(),
+        user.getPassword(), new ArrayList<>());
   }
 }
